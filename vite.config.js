baee521bd3fe5d7b6122398eaf5bf85b294d462e
chenpo <chenpo@cufa.edu.tw>
2025-11-11 14:14:34 +0800

    Fix base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: 'ghs',
-    build: {
-    outDir: 'dist'
+  base: '/ghs/',
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1000 // Optional: to handle the chunk size warning
   }
 })
